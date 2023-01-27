d119d682f32ed7b716366c03ee0a20430c51883d
acekype992 <dev@acekype.fr>
2023-01-27 03:34:33 +0100

    add player entity and first map

--- src/sfstickrpg/States/game_state_running.cpp
+++ src/sfstickrpg/States/game_state_running.cpp
@@ -1,45 +1,119 @@
 #include "game_state_running.hpp"
 
-void GameStateRunning::initialize()
-{
-    gameViewZoom_ = 0.55f;
 
-    Tileset& tileset = assets.getTileset("tileset");
-    mapByString_.insert({"map1", std::move(Map("../resources/Maps/mat1", tileset))});
+void GameStateRunning::loadMaps()
+{
+    Map map {assets.getTileset("tileset"), "../resources/maps/map0"};
+    mapByString_.insert({"map1", std::move(map)});
 
     currentMap_ = &mapByString_.find("map1")->second;
 }
 
 
+void GameStateRunning::initialize()
+{
+    loadMaps();
+
+    keyPressed_ = false;
+
+    gameViewZoom_ = 0.5f;
+
+
+
+    player_.initialize(assets.getTileset("player"));
+    player_.speed = 2.f;
+    player_.setAnim(4);
+    player_.setMapPosition(5, 5);
+}
+
+
 void GameStateRunning::pre_update()
 {
-    gameView_.setCenter(5 * 32,
-                        5 * 32);
+    movePlayer();
+
+    player_.update();
 }
 
 
 void GameStateRunning::update()
 {
     window.setView(gameView_);
+    gameView_.setCenter(player_.getPosition());
 }
 
 
 void GameStateRunning::draw()
 {
-//    window.draw(*currentMap_);
-    window.draw(currentMap_->getTileset().getDrawableTileset());
+    window.draw(*currentMap_);
+    window.draw(player_);
+}
+
+
+void GameStateRunning::movePlayer()
+{
+    if(keyPressed_ && lastKeyPressed_.get())
+    {
+        if(lastKeyPressed_->code == sf::Keyboard::Up)
+        {
+            player_.direction = Entity::Direction::up;
+            if(player_.getPosition().y > 0)
+            {
+                player_.move(0, -player_.speed);
+            }
+            player_.animOn = true;
+        }
+        else if(lastKeyPressed_->code == sf::Keyboard::Left)
+        {
+            player_.direction = Entity::Direction::left;
+            if(player_.getPosition().x > 0)
+            {
+                player_.move(-player_.speed, 0);
+            }
+            player_.animOn = true;
+        }
+        else if(lastKeyPressed_->code == sf::Keyboard::Right)
+        {
+            player_.direction = Entity::Direction::right;
+            if(player_.getPosition().x < currentMap_->getSize().x)
+            {
+                player_.move(player_.speed, 0);
+            }
+            player_.animOn = true;
+        }
+        else if(lastKeyPressed_->code == sf::Keyboard::Down)
+        {
+            player_.direction = Entity::Direction::down;
+            if(player_.getPosition().y < currentMap_->getSize().y)
+            {
+                player_.move(0, player_.speed);
+            }
+            player_.animOn = true;
+
+        }
+    }
+    else
+    {
+        player_.animOn = false;
+    }
 }
 
 
 void GameStateRunning::keyPressEvent(const Event::KeyEvent &event)
 {
-
+    keyPressed_ = true;
+    lastKeyPressed_ = std::make_unique<sf::Event::KeyEvent>(event);
 }
 
 
 void GameStateRunning::keyReleaseEvent(const Event::KeyEvent &event)
 {
-
+    if(!sf::Keyboard::isKeyPressed(sf::Keyboard::Up)
+    && !sf::Keyboard::isKeyPressed(sf::Keyboard::Left)
+    && !sf::Keyboard::isKeyPressed(sf::Keyboard::Right)
+    && !sf::Keyboard::isKeyPressed(sf::Keyboard::Down)
+    ){
+        keyPressed_ = false;
+    }
 }
 
 
--- src/sfstickrpg/States/game_state_running.hpp
+++ src/sfstickrpg/States/game_state_running.hpp
@@ -1,14 +1,18 @@
 #ifndef GAME_STATE_RUNNING_HPP
 #define GAME_STATE_RUNNING_HPP
 
-#include "Core/state_game.hpp"
-
 #include <unordered_map>
+
+#include "Core/state_game.hpp"
+#include "Entities/player_entity.hpp"
 
 #include "map.hpp"
 
+
 class GameStateRunning : public StateGame
 {
+    void loadMaps();
+
 public:
     using StateGame::StateGame;
     ~GameStateRunning() noexcept = default;
@@ -18,6 +22,9 @@ public:
     virtual void update() override;
     virtual void draw() override;
 
+private:
+    void movePlayer();
+
 protected:
     void keyPressEvent(const Event::KeyEvent &event) override;
     void keyReleaseEvent(const Event::KeyEvent &event) override;
@@ -25,13 +32,17 @@ protected:
     void mouseClickEvent(const Event::MouseButtonEvent &event) override;
     void resizeEvent(const Event::SizeEvent &event) override;
 
+    bool keyPressed_;
+    std::unique_ptr<sf::Event::KeyEvent> lastKeyPressed_;
+
 private:
     sf::View gameView_;
     float gameViewZoom_;
 
-    Tileset mapTileset_;
     std::unordered_map<std::string, Map> mapByString_;
     Map *currentMap_;
+
+    PlayerEntity player_;
 };
 
 #endif // GAME_STATE_RUNNING_HPP
